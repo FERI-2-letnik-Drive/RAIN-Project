8706b9270fa32fdbe230e4e70bb4e9c73c0b9534
Maercel <marcel.majhenic@gmail.com>
2026-05-12 15:47:22 +0200

    Init

---
+++ SmartMailBox/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Smart MailBox</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
